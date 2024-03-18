eccb16c594940b250edc64cede0c03aafaa62a04
JH <eeung025@gmail.com>
2024-03-18 21:15:46 +0900

    Delete frontend/dist directory

--- frontend/dist/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/naithar01/projects/golang/docker-desktop/frontend/app/layout\":[\"static/media/c9a5bc6a7c948fb0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
--- frontend/dist/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"eBzKOdP62hk4hU64E7dA28tpNzS00WQDrjMMjXtBK1M=\"\n}"
